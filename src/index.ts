f16490551f96c9541f831ab1d872878214157772
Ian Guimaraes <ianguimaraes31@gmail.com>
2023-04-08 17:24:37 -0300

    chore: change default port from 8000 to 8080

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ import { App } from './config/App'
 
 const server = createServer(new App().express)
 
-server.listen(8000, () => console.log('Listening'))
+server.listen(8080, () => console.log('Listening'))
